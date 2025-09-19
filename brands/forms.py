820128eecc4b985d042225216e4c3a104d39463c
KaelDucatti <ducatti860@gmail.com>
2025-09-19 20:08:42 -0300

    beautifying form template

--- brands/forms.py
+++ brands/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea, TextInput
 
 from .models import Brand
 
@@ -7,6 +7,10 @@ class BrandForm(ModelForm):
     class Meta:
         model = Brand
         fields = ["name", "description"]
+        widgets = {
+            "name": TextInput({"class": "form-control"}),
+            "description": Textarea({"class": "form-control", "rows": 3}),
+        }
 
     def clean_name(self):
         name = self.cleaned_data["name"]
